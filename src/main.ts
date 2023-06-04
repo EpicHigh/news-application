2525a0af4fe5118bdc10ee3d60955c3a56483d80
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2023-06-05 03:17:31 +0700

    refactor: clean up unused files

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { MotionPlugin } from '@vueuse/motion'
 import router from './router'
 import App from './App.vue'
 import { store } from './stores'
@@ -18,5 +17,6 @@ const vuetify = createVuetify({
 app.use(router)
 app.use(vuetify)
 app.use(store)
+app.use(MotionPlugin)
 
 app.mount('#app')
