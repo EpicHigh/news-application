0bfbf0d2b59608fc73926470e053683fe8e66d38
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2023-06-04 20:00:32 +0700

    chore: initialize a project

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+const createAliasSetting = require('@vue/eslint-config-airbnb-with-typescript/createAliasSetting')
+const path = require('path')
+
+module.exports = {
+  root: true,
+  plugins: ['prettier'],
+  'extends': [
+    'plugin:vue/vue3-essential',
+    '@vue/eslint-config-airbnb-with-typescript',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    project: path.join(__dirname, './tsconfig.eslint.json'),
+  },
+  rules: {
+    'prettier/prettier': ['error'],
+  },
+  settings: {
+    ...createAliasSetting([path.join(__dirname, './tsconfig.eslint.json')])
+  }
+}
