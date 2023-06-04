7bde436cb6dff408318134886119087e5b3e05f8
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2023-06-05 03:17:31 +0700

    refactor: clean up unused files
    (cherry picked from commit 2525a0af4fe5118bdc10ee3d60955c3a56483d80)

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,11 +1,9 @@
 import axios from 'axios'
 
-console.log(import.meta.env, 'import.meta.env')
-
 export default axios.create({
   baseURL: import.meta.env.VITE_NEWS_API_BASE_URL,
   params: {
     apiKey: import.meta.env.VITE_NEWS_API_KEY,
-    country: 'th'
+    country: 'us'
   }
 })
