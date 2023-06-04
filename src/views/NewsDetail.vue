365d78a9fc881c6c2f9862789383f6144824f878
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2023-06-05 05:07:40 +0700

    fix: fix news detail responsive
    (cherry picked from commit 94b687151adb63b776262bc849557b819c97452a)

--- src/views/NewsDetail.vue
+++ src/views/NewsDetail.vue
@@ -26,22 +26,20 @@ const backgroundColor = computed(() => {
 </script>
 
 <template>
-  <div v-if="article">
-    <v-sheet class="text-white" :color="backgroundColor" height="100vh">
-      <v-container>
-        <h1 class="title my-6">{{ article.title }}</h1>
-        <p class="date">{{ format(new Date(article.publishedAt), 'd MMMM yyyy') }}</p>
-        <p class="content">{{ article.description }}</p>
-        <p class="content">{{ article.content }}</p>
-      </v-container>
-      <v-img
-        class="image w-100"
-        v-if="article.urlToImage"
-        :src="article.urlToImage"
-        :alt="article.description || ''"
-      />
-    </v-sheet>
-  </div>
+  <v-sheet v-if="article" class="text-white" :color="backgroundColor" height="100vh">
+    <v-container>
+      <h1 class="title my-6">{{ article.title }}</h1>
+      <p class="date">{{ format(new Date(article.publishedAt), 'd MMMM yyyy') }}</p>
+      <p class="content">{{ article.description }}</p>
+      <p class="content">{{ article.content }}</p>
+    </v-container>
+    <v-img
+      class="image w-100"
+      v-if="article.urlToImage"
+      :src="article.urlToImage"
+      :alt="article.description || ''"
+    />
+  </v-sheet>
 </template>
 
 <style scoped>
@@ -49,15 +47,23 @@ const backgroundColor = computed(() => {
   font-weight: bolder;
   text-transform: uppercase;
   letter-spacing: 0.1rem;
-  font-size: 2.4rem;
+  font-size: 1.6rem;
+
+  @media (min-width: 768px) {
+    font-size: 2.4rem;
+  }
 }
 
 .date {
-  font-size: 1.6rem;
+  font-size: 1.4rem;
   font-weight: bold;
   line-height: 1.5;
   letter-spacing: 0.2rem;
   text-transform: uppercase;
+
+  @media (min-width: 768px) {
+    font-size: 1.8rem;
+  }
 }
 
 .content {
@@ -68,5 +74,9 @@ const backgroundColor = computed(() => {
 .image {
   position: absolute;
   bottom: 0;
+  width: 100%;
+  height: auto;
+  max-height: 70vh;
+  object-fit: cover;
 }
 </style>
