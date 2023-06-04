c22cb3ca5b04479464441ccb9741d73e90c77ce6
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2023-06-04 21:14:04 +0700

    chore: initialize vuetify and vuex
    (cherry picked from commit 67d0321af043c725a487ef0d52e1955f236a3a35)

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 import router from './router'
 import App from './App.vue'
+import store from './stores'
 
 const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives
+})
 
 app.use(router)
+app.use(vuetify)
+app.use(store)
 
 app.mount('#app')
---
+++ src/stores/index.ts
@@ -0,0 +1,10 @@
+// @ts-ignore https://github.com/trim21/vuex-ts-bug
+import { createStore } from 'vuex'
+
+export default createStore({
+  state() {
+    return {}
+  },
+  mutations: {},
+  actions: {}
+})
