0c48fcf54eb4f4831fdd86af7e9a91e831fed715
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2023-06-05 19:23:47 +0700

    chore: setup mdi icons

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { MotionPlugin } from '@vueuse/motion'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
 import router from './router'
 import App from './App.vue'
 import { store } from './stores'
@@ -11,7 +13,14 @@ import { store } from './stores'
 const app = createApp(App)
 const vuetify = createVuetify({
   components,
-  directives
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  }
 })
 
 app.use(router)
