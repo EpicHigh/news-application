9a80d10f751f30bdad85b63f5480dd3c93125af4
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2023-06-05 19:23:09 +0700

    feat: introduce an appbar

---
+++ src/components/AppBar/AppBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-app-bar>
+    <v-toolbar-title>News App</v-toolbar-title>
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>mdi-filter</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<style scoped></style>
