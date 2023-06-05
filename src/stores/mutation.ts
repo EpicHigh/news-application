b3f6cd394b1e52c10f95deba5c427a293085b813
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2023-06-05 19:24:25 +0700

    feat: introduce a new updating top heading title mutation

--- src/stores/mutation.ts
+++ src/stores/mutation.ts
@@ -1,16 +1,19 @@
 import type { Article } from '@/types'
+import type { TopHeadlinePayload } from './action'
 import type { State } from './state'
 
 export enum MutationTypes {
   SET_ERROR = 'SET_ERROR',
   SET_LOADING = 'SET_LOADING',
-  SET_TOP_HEADLINES = 'SET_TOP_HEADLINES'
+  SET_TOP_HEADLINES = 'SET_TOP_HEADLINES',
+  UPDATE_TOP_HEADLINE_TITLE = 'UPDATE_TOP_HEADLINE_TITLE'
 }
 
 export type Mutations<S = State> = {
   [MutationTypes.SET_ERROR](state: S, payload: string): void
   [MutationTypes.SET_LOADING](state: S, payload: boolean): void
   [MutationTypes.SET_TOP_HEADLINES](state: S, payload: Article[]): void
+  [MutationTypes.UPDATE_TOP_HEADLINE_TITLE](state: S, payload: TopHeadlinePayload): void
 }
 
 export const mutations: Mutations = {
@@ -22,5 +25,8 @@ export const mutations: Mutations = {
   },
   [MutationTypes.SET_TOP_HEADLINES](state, payload) {
     state.topHeadlines = payload
+  },
+  [MutationTypes.UPDATE_TOP_HEADLINE_TITLE](state, payload) {
+    state.topHeadlines[payload.id].title = payload.title
   }
 }
