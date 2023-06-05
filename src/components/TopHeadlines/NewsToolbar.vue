62a9efd17c22555d356819b0cf10202723c27ce3
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2023-06-05 19:26:39 +0700

    feat: introduce a news toolbar
    (cherry picked from commit 2659bf6b3e267f1524c03a99c3788cae893589bd)

---
+++ src/components/TopHeadlines/NewsToolbar.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useStore } from 'vuex'
+import { Store } from '@/stores'
+import { ActionTypes } from '@/stores/action'
+
+interface Props {
+  title: string
+  id: number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  title: '',
+  id: 0
+})
+
+const router = useRouter()
+const dialog = ref(false)
+const menu = ref(false)
+const valid = ref(true)
+const newTitle = ref(props.title)
+const titleRules = [(v) => v.length <= 255 || 'Title must be less than 255 characters']
+const store = useStore<Store>()
+
+function closeDialog() {
+  dialog.value = false
+}
+
+function openDialog() {
+  dialog.value = true
+}
+
+function saveContent() {
+  closeDialog()
+  store.dispatch(ActionTypes.UPDATE_TOP_HEADLINE_TITLE, { id: props.id, title: newTitle.value })
+}
+
+function goToDetail() {
+  router.push({ name: 'NewsDetail', params: { id: props.id } })
+}
+</script>
+
+<template>
+  <v-menu v-model="menu">
+    <template #activator="{ props }">
+      <v-btn v-bind="props" icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </template>
+    <!-- menu content goes here -->
+    <v-btn @click="goToDetail">Read</v-btn>
+    <v-btn @click="openDialog">Edit</v-btn>
+  </v-menu>
+
+  <v-dialog v-model="dialog" max-width="500px">
+    <v-form v-model="valid" @submit.prevent="saveContent" @reset="closeDialog">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Modify Title</span>
+        </v-card-title>
+
+        <v-card-text>
+          <v-textarea
+            v-model="newTitle"
+            :rules="titleRules"
+            auto-grow
+            label="New title"
+            placeholder="Enter new title here"
+          ></v-textarea>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text type="reset">Close</v-btn>
+          <v-btn :disabled="!valid" color="blue darken-1" text type="submit">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-form>
+  </v-dialog>
+</template>
+
+<style scoped></style>
